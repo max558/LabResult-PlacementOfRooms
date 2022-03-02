e0b45bd48e56e783de67168bf47b647b55bf238e
mivvlk <mivvlk@yandex.ru>
2022-03-02 23:20:28 +0300

    Добавьте файлы проекта.

---
+++ PlacementRooms.cs
@@ -0,0 +1,122 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using RevitAPITreningLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedPlacementOfRooms
+{
+    /*
+     * Разработать плагин, позволяющий автоматизировать расстановку помещений и марок помещений в модели. 
+     * Марка должна иметь формат "номерЭтажа_номерПомещения", например, 1_20 - первый этаж, 20-ое помещение.. 
+     * Модель можно разработать самостоятельно или использовать готовую модель из предыдущих заданий.
+     * 
+     * В данном приложении используется семейство Room Tag_new
+     */
+    [Transaction(TransactionMode.Manual)]
+    public class PlacementRooms : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            List<Level> levelList = SelectionUtils.SelectAllElement<Level>(doc);
+
+            if (levelList.Count < 1)
+            {
+                return Result.Failed;
+            }
+
+
+            //Проверка наличия маркировки помещений
+            RoomTagType tagSymbol = (SelectionUtils.SelectAllElementCategoryType<RoomTagType>(doc, BuiltInCategory.OST_RoomTags))
+                .Where(x => x.FamilyName.Equals("Room Tag_new")).FirstOrDefault();
+
+            if (tagSymbol == null)
+            {
+                TaskDialog.Show("Ошибка", "Семейство \"Room Tag_new\" не найдено. Поэтому будут вставлены типовые марки помещений и нужный ответ не будет сформирован.");
+            }
+            else
+            {
+                using (Transaction ts = new Transaction(doc, "Активация семейства"))
+                {
+                    ts.Start();
+                    if (!tagSymbol.IsActive)
+                    {
+                        tagSymbol.Activate();
+                        doc.Regenerate();
+                    }
+                    ts.Commit();
+                }
+            }
+
+            Parameter phasePram = doc.ActiveView.get_Parameter(BuiltInParameter.VIEW_PHASE);
+            Phase phase = doc.GetElement(phasePram.AsElementId()) as Phase;
+            if (phasePram.StorageType != StorageType.ElementId)
+            {
+                return Result.Failed;
+            }
+
+            List<Room> projectRooms = new List<Room>();
+            for (int i = 0; i < levelList.Count; i++)
+            {
+                int indexLevel = i + 1; // Подвал и нижние этажы в нумирации не учитываются
+                List<Room> listRoomLevel = InsertNewRoomInPlanCircuit(doc, levelList[i], phase, indexLevel.ToString());
+
+                //Собираем общий список созданных помещений (для дальнейшей обработки)
+                foreach (var room in listRoomLevel)
+                {
+                    projectRooms.Add(room);
+                }
+            }
+
+            return Result.Succeeded;
+        }
+
+
+        List<Room> InsertNewRoomInPlanCircuit(Document doc, Level level, Phase constructionPhase, string nameRoomLevel)
+        {
+            List<Room> resRoom = new List<Room>();
+            using (Transaction tr = new Transaction(doc, "Создание комнаты на плане"))
+            {
+                tr.Start();
+                // Получение топологии на уровне
+                PlanTopology planTopology = doc.get_PlanTopology(level);
+                int i = 1;
+                foreach (PlanCircuit circuit in planTopology.Circuits)
+                {
+                    //на наличие контура и отсутсвие в нем комнаты
+                    if (circuit != null
+                        && !circuit.IsRoomLocated)
+                    {
+                        //Создание не размещенного помещения (строчки в СП)
+                        Room newScheduleRoom = doc.Create.NewRoom(constructionPhase);
+                        newScheduleRoom.Name = "Помещение " + i.ToString();
+                        //newScheduleRoom.Number = nameRoomLevel + "_" + i.ToString();
+
+                        Room newRoom2 = null;
+                        newRoom2 = doc.Create.NewRoom(newScheduleRoom, circuit);
+
+                        if (null != newRoom2)
+                        {
+                            resRoom.Add(newRoom2);
+                            //Запись в комментарий значения этажа
+                            Parameter commentParam = newRoom2.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
+                            commentParam.Set(nameRoomLevel);
+                        }
+                    }
+                    i++;
+                }
+                tr.Commit();
+            }
+
+            return resRoom;
+        }
+    }
+
+}
---
+++ Библиотека/SelectionUtils.cs
@@ -0,0 +1,220 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITreningLibrary
+{
+    public class SelectionUtils
+    {
+        /*
+         *** ==== Выбор всех типовых элементов модели по классу ==== ****
+         * Входные параметры:
+         * commandData - внешняя команда
+         * Вывод: список элементов
+         */
+        public static List<T> SelectAllElement<T>(Document doc
+            //ExternalCommandData commandData
+            )
+        {
+            /*
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            */
+
+            List<T> list = new FilteredElementCollector(doc)
+                    .OfClass(typeof(T))
+                    .OfType<T>()
+                    //.Cast<T>()
+                    .ToList();
+
+            return list;
+        }
+
+        /*
+         *** ==== Выбор всех типовых элементов модели по классу в активном представлении ==== ****
+         * Входные параметры:
+         * commandData - внешняя команда
+         * viewId - ID представления. По умолчанию NULL - в активном документе
+         * Вывод: список элементов
+         */
+        public static List<T> SelectAllElementActiveDoc<T>(
+            Document doc,
+            //ExternalCommandData commandData, 
+            ElementId viewId = null)
+        {
+            /*
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            */
+
+            List<T> list = new List<T>();
+            if (viewId == null)
+            {
+                list = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                    .OfClass(typeof(T))
+                    .OfType<T>()
+                    //.Cast<T>()
+                    .ToList();
+            }
+            else
+            {
+                list = new FilteredElementCollector(doc, viewId)
+                .OfClass(typeof(T))
+                .OfType<T>()
+                //.Cast<T>()
+                .ToList();
+            }
+
+            return list;
+        }
+
+        /*
+         * Выбор всех экземпляров по категории
+         */
+        public static List<T> SelectAllElementCategory<T>(Document doc
+            //ExternalCommandData commandData
+            , BuiltInCategory builtInCategory)
+        {
+            /*
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            */
+
+            List<T> list = new FilteredElementCollector(doc)
+                .OfCategory(builtInCategory)
+                .WhereElementIsNotElementType()
+                .OfType<T>()
+                //.Cast<T>()
+                .ToList();
+
+            return list;
+        }
+
+        /*
+         * Выбор всех типов семейств по категории
+         */
+        public static List<T> SelectAllElementCategoryType<T>(Document doc
+           // ExternalCommandData commandData
+            , BuiltInCategory builtInCategory)
+        {
+            /*
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            */
+           
+            List<T> list = new FilteredElementCollector(doc)
+                .OfCategory(builtInCategory)
+                .WhereElementIsElementType()
+                .OfType<T>()
+                //.Cast<T>()
+                .ToList();
+
+            return list;
+        }
+
+        /*
+         * Выбор элемента
+         */
+        public static Element PickObject(ExternalCommandData commandData, string message = "Выберите элемент:")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, message);
+            var oElement = doc.GetElement(selectedObject);
+            return oElement;
+        }
+
+        /*
+         * Множественный выбор элемента
+         */
+        public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы:")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
+            List<Element> elementList = new List<Element>();
+            try
+            {
+                elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            { }
+
+            return elementList;
+        }
+
+        /*
+         * Выбор объекта
+         */
+        public static T GetObject<T>(ExternalCommandData commandData, string message = "Выберите объект:")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            
+            Reference selectedObj = null;
+
+            T elem;
+            try
+            {
+                selectedObj = uidoc.Selection.PickObject(ObjectType.Element, message);
+            }
+            catch (Exception)
+            {
+
+                return default(T);
+            }
+            elem = (T)(object)doc.GetElement(selectedObj.ElementId);
+            return elem;
+        }
+
+        /*
+         * Выбор множества точек
+         * message - Строковое сообщение
+         * objectSnapTypes - выбор типа точек
+         * counPoint - выбор количеста точек (0 - это выбор до отмены или ESC)
+         */
+        public static List<XYZ> GetPoints(ExternalCommandData commandData, string message, ObjectSnapTypes objectSnapTypes, int counPoint = 0)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            List<XYZ> points = new List<XYZ>();
+            bool loopCondidtion = true;
+
+            while (loopCondidtion)
+            {
+                XYZ pickedPoint = null;
+                try
+                {
+                    pickedPoint = uidoc.Selection.PickPoint(objectSnapTypes, message);
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+                {
+                    break;
+                }
+                points.Add(pickedPoint);
+                //Проверка выбора количества точек
+                if (counPoint > 0)
+                {
+                    loopCondidtion = !(points.Count == counPoint);
+                }
+            }
+
+            return points;
+        }
+    }
+}
